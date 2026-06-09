dcdf60192c0e88b7772e379aa39433e86fe95092
OmegaPoteto <126129378+OmegaPoteto@users.noreply.github.com>
2026-06-09 15:42:43 -0500

    Update bike_trace.hpp

--- source/program/patches/bike_trace.hpp
+++ source/program/patches/bike_trace.hpp
@@ -1,35 +1,27 @@
 #include "lib.hpp"
 #include <lib/log/logger_mgr.hpp>
+#include <lib/util/modules.hpp>
 
-// Trace build: logs [BIKETRACE] lines so we can see what the bike-mount triggers.
-// Startup line proves logging works; RequestPlayerRideBicycle proves hooks fire.
+// Model trace: logs every model-by-hash load (hash + caller offset) plus a
+// RideFunc marker. Mount the bike, then find the LoadModel lines right after
+// the [BIKETRACE] RideFunc_d978a0 marker -- that hash/caller is the bike model.
 
-#define BIKE_TRACE(NAME, LABEL)                                          \
-    HOOK_DEFINE_INLINE(NAME) {                                           \
-        static void Callback(exl::hook::nx64::InlineCtx*) {              \
-            ::Logging.Log("[BIKETRACE] " LABEL "\n");            \
-        }                                                                \
-    };
+HOOK_DEFINE_INLINE(Trace_RideFunc) {
+    static void Callback(exl::hook::nx64::InlineCtx*) {
+        ::Logging.Log("[BIKETRACE] RideFunc_d978a0 (MOUNT)\n");
+    }
+};
 
-BIKE_TRACE(Trace_RequestRideBicycle,    "RequestPlayerRideBicycle")
-BIKE_TRACE(Trace_RideFunc,              "RideFunc_d978a0")
-BIKE_TRACE(Trace_GetDressupParts,       "GetPlayerDressupParts")
-BIKE_TRACE(Trace_SetDressupParts,       "SetPlayerDressupParts")
-BIKE_TRACE(Trace_SetDressupPartsPreset, "SetPlayerDressupPartsByPreset")
-BIKE_TRACE(Trace_SetNoDressupParts,     "SetPlayerNoDressupParts")
-BIKE_TRACE(Trace_AddDressupItemPreset,  "PlayerAddDressupItemByPreset")
-BIKE_TRACE(Trace_ChangeBicycle,         "ChangeBicycle")
-BIKE_TRACE(Trace_ModelHandler,          "ModelHandler_de02d0")
+HOOK_DEFINE_INLINE(Trace_LoadModel) {
+    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
+        ::Logging.Log("[BIKETRACE] LoadModel hash=%016lx caller=%lx\n",
+            ctx->X[1],
+            ctx->X[30] - exl::util::modules::GetTargetStart());
+    }
+};
 
 void install_bike_trace_patch() {
-    ::Logging.Log("[BIKETRACE] trace mod loaded\n");
-    Trace_RequestRideBicycle::InstallAtOffset(0x148b960);
+    ::Logging.Log("[BIKETRACE] model trace loaded\n");
     Trace_RideFunc::InstallAtOffset(0xd978a0);
-    Trace_GetDressupParts::InstallAtOffset(0x1472580);
-    Trace_SetDressupParts::InstallAtOffset(0x1472680);
-    Trace_SetDressupPartsPreset::InstallAtOffset(0x1472830);
-    Trace_SetNoDressupParts::InstallAtOffset(0x14727a0);
-    Trace_AddDressupItemPreset::InstallAtOffset(0x14adde0);
-    Trace_ChangeBicycle::InstallAtOffset(0x1472a60);
-    Trace_ModelHandler::InstallAtOffset(0xde02d0);
+    Trace_LoadModel::InstallAtOffset(0x1411030);
 }
